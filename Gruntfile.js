62ae0b15b859678b360ea784a16de6bb7fbd840c
thmain <tutorialhorizon@gmail.com>
2014-08-27 23:26:10 -0700

    Initial commit

---
+++ Gruntfile.js
@@ -0,0 +1,15 @@
+module.exports = function(grunt) {
+
+  grunt.initConfig({});
+
+  grunt.config( 'webpack', require('./grunt/webpack.js') );
+  grunt.config( 'react', require('./grunt/react.js') );
+  grunt.config( 'watch', require('./grunt/watch.js') );
+
+  grunt.loadNpmTasks('grunt-webpack');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-react');
+  
+  grunt.registerTask('default', ['react']);
+
+};
---
+++ README.md
@@ -0,0 +1,10 @@
+React Kit
+---
+
+A example application that watches your React jsx files, compiles them and then bundles them using webpack
+
+- Webpack
+- React
+- Grunt tasks to build and watch react and keep the nodeserver running using nodemon
+- Preconfigured settings so that react shows up in your chrome dev tools
+- A basic nodejs server based on expressjs 4 that serves your static html pages
